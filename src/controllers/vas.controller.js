18c6a6962a9d214a6b1c570d517f166641b405d7
sodiq <abdulrazaqsodiq2015@gmail.com>
2023-08-24 13:21:42 +0100

    consume Api's

--- src/controllers/vas.controller.js
+++ src/controllers/vas.controller.js
@@ -2,6 +2,7 @@ const husmoServices = require("../services/husmo.services");
 
 module.exports = {
   data: async (req, res) => {
+   
     let result = await husmoServices.data(req, res);
     console.log(result);
     res.send(result);
@@ -16,4 +17,25 @@ module.exports = {
     console.log(result);
     res.send(result);
   },
+  validateMeter: async (req, res) => {
+    let result = await husmoServices.validateMeter(req, res);
+    console.log(result);
+    res.send(result);
+  },
+  validateIUC: async (req, res) => {
+    let result = await husmoServices.validateIUC(req, res);
+    console.log(result);
+    res.send(result);
+  },
+  cablesub: async (req, res) => {
+    let result = await husmoServices.cablesub(req, res);
+    console.log(result);
+    res.send(result);
+  },
+  electric: async (req, res) => {
+    let result = await husmoServices.electric(req, res);
+    console.log(result);
+    res.send(result);
+  }
+  
 };
