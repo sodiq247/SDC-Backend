a718b0917b769dc4084a3cf91bec5932f4c86944
sodiq <abdulrazaqsodiq2015@gmail.com>
2023-10-04 09:40:54 +0100

    redirecting error

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,3 +1,8 @@
+const db = require('../../models/index')
+const { User, Profile, Role, UserRole, Wallet } = db;
+const walletService = require('../services/wallet.service')
+
+
 var express = require('express');
 var router = express.Router();
 
@@ -6,4 +11,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get("/dashboard", async (req, res) => {
+  let availableBal = await walletService.availableBalance(userid)
+  let wallet = await walletService.userWallet(userid);
+
+  res.render("user/Dashboard", { 
+    title: "Dashboard",
+    layout: "Dashboard",
+    wallet,
+    availableBal,
+  })
+})
 module.exports = router;
